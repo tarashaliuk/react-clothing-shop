effc13632a382844594e01532b17519608615e6f
Taras Haliuk <tarashaliuk@gmail.com>
2020-05-30 18:08:54 +0200

    created sign in form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+import './App.css';
+
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-
-import './App.css';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
       <Header/>
       <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route path='/shop' component={ShopPage}/>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
